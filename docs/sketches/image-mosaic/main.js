79405b050d86ad2d3e79a4a33c90166f15684771
Juan Camilo Florez <jcflorezv@unal.edu.co>
2021-04-01 21:50:42 -0500

    photographic mosaic with euclidean distance euristic

--- docs/sketches/image-mosaic/main.js
+++ docs/sketches/image-mosaic/main.js
@@ -2,7 +2,10 @@ var MODE = 'LOCAL'
 let pics = [];
 let picLocRegex = '/vc/docs/sketches/image-mosaic/?.webp';
 let bigPictureLoc = '/vc/docs/sketches/image-mosaic/bigpicture.jpg';
-let paletteSize = 16
+let averages = [];
+let paletteSize = 16;
+var tileSize = 20;
+var mos;
 
 function preload() {
   if (MODE == 'LOCAL') {
@@ -18,36 +21,74 @@ function preload() {
 }
 
 function setup() {
+  bigPicture.resize(1000, 1000);
   createCanvas(bigPicture.width, bigPicture.height);
-  image(bigPicture, 0, 0)
-  w = 0
-  for (i = 1; i < paletteSize; i++) {
-    pics[i].resize(30, 30);
-    image(pics[i], w, 0);
-    w += pics[i].width;
+  storePics();
+  showMosaic();
+}
+
+function showMosaic() {
+  for (let y = 0; y < bigPicture.height; y = y + tileSize) {
+    for (let x = 0; x < bigPicture.width; x = x + tileSize) {
+      let tile = bigPicture.get(x, y, tileSize - 1, tileSize - 1);
+      [r, g, b] = getAverageColor(tile);
+      let i = getSimilarColor(r, g, b);
+      pics[i].resize(tileSize, tileSize);
+      image(pics[i], x, y);
+    }
   }
 }
 
-function draw() {
-
+function showAverages() {
+  for (let y = 0; y < bigPicture.height; y = y + tileSize) {
+    for (let x = 0; x < bigPicture.width; x = x + tileSize) {
+      let tile = bigPicture.get(x, y, tileSize - 1, tileSize - 1);
+      [r, g, b] = getAverageColor(tile);
+      let c = color(r, g, b);
+      fill(c)
+      square(x, y, tileSize);
+    }
+  }
 }
 
-function applyKernel(img) {
-    img.loadPixels()
-    for (let y = 0; y < img.height; y++) {
-      for (let x = 0; x < img.width; x++) {
-        let i = (x + y * width)*4;
-        let result = filterfunc(img.pixels[i + 0], img.pixels[i + 1], img.pixels[i + 2], img.pixels[i + 3]);
-        let r = result[0];
-        let g = result[1];
-        let b = result[2];
-        let a = result[3];
-  
-        img.pixels[i + 0] = r;
-        img.pixels[i + 1] = g;
-        img.pixels[i + 2] = b;
-        img.pixels[i + 3] = a;
-      }
+function getSimilarColor(r, g, b) {
+  let idx = 0;
+  let ans = -1;
+  let min = Math.min();
+  for (avg of averages) {
+    let d =  (avg[0] - r)*(avg[0] - r)
+           + (avg[1] - g)*(avg[1] - g)
+           + (avg[2] - b)*(avg[2] - b);
+    if (d < min) {
+      min = d;
+      ans = idx;
     }
-    img.updatePixels()
+    idx++;
+  }
+  return ans;
+}
+
+function storePics() {
+  for (i = 1; i < paletteSize; i++) {
+    pics[i].resize(tileSize, tileSize);
+    averages.push(getAverageColor(pics[i]))
+  }
+}
+
+function getAverageColor(pic) {
+  pic.loadPixels()
+  let r = 0;
+  let g = 0;
+  let b = 0;
+  let n = 0;
+  for (let y = 0; y < pic.height; y++) {
+    for (let x = 0; x < pic.width; x++) {
+      let i = (x + y * pic.width)*4;
+      r += pic.pixels[i + 0];
+      g += pic.pixels[i + 1];
+      b += pic.pixels[i + 2];
+      n++;
+    }
+  }
+  return [r/n, g/n, b/n];
 }
